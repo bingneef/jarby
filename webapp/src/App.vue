ef5693d74859d2f9dbe323f24f558dd4939f164a
Bing Steup <b.steup@sping.nl>
2017-02-26 13:04:30 +0100

    The navheader should be fixed.

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
-    <app-header></app-header>
-    <router-view></router-view>
+    <div class="header">
+      <app-header></app-header>
+    </div>
+    <div class="content">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
@@ -17,14 +21,28 @@ export default {
 }
 </script>
 
-<style lang="sass">
-  .no-scrolling
-    overflow: hidden
+<style lang="sass" scoped>
+.no-scrolling
+  overflow: hidden
 
-  @import './stylesheets/theme_overrides.sass'
-  @import './stylesheets/vars.sass'
-  @import './stylesheets/mixins.sass'
+@import './stylesheets/theme_overrides.sass'
+@import './stylesheets/vars.sass'
+@import './stylesheets/mixins.sass'
 
-  $fa-font-path: "~font-awesome/fonts"
-  @import "~font-awesome/scss/font-awesome"
+$fa-font-path: "~font-awesome/fonts"
+@import "~font-awesome/scss/font-awesome"
+
+#app
+  position: fixed
+  top: 0
+  right: 0
+  bottom: 0
+  left: 0
+  display: flex
+  flex-direction: column
+
+  .content
+    flex: 1
+    overflow: scroll
+
 </style>
